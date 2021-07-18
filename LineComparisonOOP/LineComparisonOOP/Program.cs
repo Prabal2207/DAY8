2acc360d6a8233db78d831ad683108623f54f2c5
Prabal2207 <prabal760@gmail.com>
2021-07-18 11:07:13 +0530

    [Prabal] Added:-Main function of line comparison

---
+++ LineComparisonOOP/LineComparisonOOP/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LineComparisonOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // UC1_LengthofLine obj = new UC1_LengthofLine();
+            //UC2_Checktwolineequal.Checkequal();
+            // UC3_checklengthbasedon_endpoints.comparelength();
+
+            UC4_Compareto obj = new UC4_Compareto();
+            obj.Cal();
+            obj.compare();
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
