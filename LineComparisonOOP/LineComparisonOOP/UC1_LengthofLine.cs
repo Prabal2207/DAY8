8f65aafe317d66645bb80dc0bd2ce741547fd043
Prabal2207 <prabal760@gmail.com>
2021-07-18 11:09:37 +0530

    [Prabal] Added:-Coordinates value is taken and length of line is calculated

---
+++ LineComparisonOOP/LineComparisonOOP/UC1_LengthofLine.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace LineComparisonOOP
+{
+    public class UC1_LengthofLine
+    {
+        public  void Cal_length()
+        {
+
+            Console.WriteLine("Enter x1:");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter y1:");
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter x2:");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter y2:");
+            int y2 = Convert.ToInt32(Console.ReadLine());
+
+            Double formula = (x2 - x1) ^ 2 + (y2 - y1) ^ 2;
+            Double Length_of_Line = Math.Sqrt(formula);
+
+            Console.WriteLine("Length of line is:" + Length_of_Line);
+
+
+        }
+    }
+}
